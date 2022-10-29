283fc43a0fdcd18086b9402ed737624c60622587
sollomka <solomiia.holovata.ri.2022@lpnu.ua>
2022-10-29 23:48:23 +0300

    first

---
+++ АП_РІ_11_Головата_ЛР_04_6/AP_Lab_04_6.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double P, S;
+	int i, k;
+
+	P = 1;
+	i = 1;
+
+	while (i <= 10)
+	{
+		S = 0;
+		k = 1;
+		while (k <= i)		{			S += 1. / k;			k++;		}		P *= (i + S) / sqrt(S);		i++;
+	}
+	cout << P << endl;
+
+	P = 1;
+	i = 1;
+	
+	do {
+		S = 0;
+		k = 1;
+		do {			S += 1. / k;			k++;
+		} while (k <= i);
+		P *= (i + S) / sqrt(S);		i++;
+	} while (i <= 10);
+	cout << P << endl;
+
+	P = 1;
+	for (i = 1; i <= 10; i++)	{
+		S = 0;
+			for (k = 1; k <= i; k++)
+			{
+				S += 1. / k;
+		}
+			P *= (i + S) / sqrt(S);
+	}
+	cout << P << endl;
+
+	P = 1;
+	for (i = 10; i >= 1; i--)
+	{
+		S = 0;
+		for (k = i; k >= 1; k--)
+		{
+			S += 1. / k;
+		}
+		P *= (i + S) / sqrt(S);
+	}
+	cout << P << endl;
+	return 0;
+
+	}
+
+
+
